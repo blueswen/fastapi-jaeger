9fa632a1104b186afb5ae643a36d0f1a43d1fb6d
blueswen <blueswen.tw@gmail.com>
2023-07-09 13:52:18 +0800

    Update dependencies

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -8,10 +8,12 @@ import httpx
 import uvicorn
 from fastapi import FastAPI, Response
 from opentelemetry import trace
-from opentelemetry.exporter.jaeger.proto.grpc import \
-    JaegerExporter as GrpcJaegerExporter
-from opentelemetry.exporter.jaeger.thrift import \
-    JaegerExporter as ThriftJaegerExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter as OTLPSpanExporterGRPC,
+)
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+    OTLPSpanExporter as OTLPSpanExporterHTTP,
+)
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.propagate import inject
@@ -23,20 +25,17 @@ from starlette.types import ASGIApp
 APP_NAME = os.environ.get("APP_NAME", "app")
 EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 8000)
 
-# grpc, thrift-collector,  thrift-agent
-MODE = os.environ.get("MODE", "grpc")
+# jaeger-grpc, jaeger-http, otel-collector-grpc, otel-collector-http
+MODE = os.environ.get("MODE", "otel-collector-grpc")
 
-# with grpc through jaeger collector
-COLLECTOR_ENDPOINT_GRPC_ENDPOINT = os.environ.get(
-    "COLLECTOR_ENDPOINT_GRPC_ENDPOINT", "jaeger-collector:14250")
-
-# with thrift through jaeger collector
-COLLECTOR_THRIFT_URL = os.environ.get(
-    "COLLECTOR_THRIFT_URL", "http://jaeger-collector:14268")
-
-# with thrift through jaeger agent
-AGENT_HOST_NAME = os.environ.get("AGENT_HOST_NAME", "jaeger-agent")
-AGENT_PORT = int(os.environ.get("AGENT_PORT", 6831))
+JAEGER_GRPC_ENDPOINT = os.environ.get("JAEGER_GRPC_ENDPOINT", "jaeger-collector:4317")
+JAEGER_HTTP_ENDPOINT = os.environ.get(
+    "JAEGER_HTTP_ENDPOINT", "http://jaeger-collector:4318/v1/traces"
+)
+OTEL_GRPC_ENDPOINT = os.environ.get("OTEL_GRPC_ENDPOINT", "otel-collector:4317")
+OTEL_HTTP_ENDPOINT = os.environ.get(
+    "OTEL_HTTP_ENDPOINT", "http://otel-collector:4318/v1/traces"
+)
 
 TARGET_ONE_HOST = os.environ.get("TARGET_ONE_HOST", "app-b")
 TARGET_TWO_HOST = os.environ.get("TARGET_TWO_HOST", "app-c")
@@ -47,28 +46,39 @@ app = FastAPI()
 def setting_jaeger(app: ASGIApp, app_name: str, log_correlation: bool = True) -> None:
     # Setting jaeger
     # set the service name to show in traces
-    resource = Resource.create(attributes={
-        "service.name": app_name
-    })
+    resource = Resource.create(attributes={"service.name": app_name})
 
     # set the tracer provider
     tracer = TracerProvider(resource=resource)
     trace.set_tracer_provider(tracer)
 
-    # use different mode to push span according environment variable MODE
-    if MODE == "thrift-collector":
-        tracer.add_span_processor(BatchSpanProcessor(ThriftJaegerExporter(
-            collector_endpoint=f'{COLLECTOR_THRIFT_URL}/api/traces?format=jaeger.thrift',
-        )))
-    elif MODE == "thrift-agent":
-        tracer.add_span_processor(BatchSpanProcessor(ThriftJaegerExporter(
-            agent_host_name=AGENT_HOST_NAME,
-            agent_port=AGENT_PORT,
-        )))
+    if MODE == "jaeger-grpc":
+        tracer.add_span_processor(
+            BatchSpanProcessor(
+                OTLPSpanExporterGRPC(endpoint=JAEGER_GRPC_ENDPOINT, insecure=True)
+            )
+        )
+    elif MODE == "jaeger-http":
+        tracer.add_span_processor(
+            BatchSpanProcessor(OTLPSpanExporterHTTP(endpoint=JAEGER_HTTP_ENDPOINT))
+        )
+    elif MODE == "otel-collector-grpc":
+        tracer.add_span_processor(
+            BatchSpanProcessor(
+                OTLPSpanExporterGRPC(endpoint=OTEL_GRPC_ENDPOINT, insecure=True)
+            )
+        )
+    elif MODE == "otel-collector-http":
+        tracer.add_span_processor(
+            BatchSpanProcessor(OTLPSpanExporterHTTP(endpoint=OTEL_HTTP_ENDPOINT))
+        )
     else:
-        # grpc (default)
-        tracer.add_span_processor(BatchSpanProcessor(GrpcJaegerExporter(
-            collector_endpoint=COLLECTOR_ENDPOINT_GRPC_ENDPOINT, insecure=True)))
+        # default otel-collector-grpc
+        tracer.add_span_processor(
+            BatchSpanProcessor(
+                OTLPSpanExporterGRPC(endpoint=OTEL_GRPC_ENDPOINT, insecure=True)
+            )
+        )
 
     # override logger format which with trace id and span id
     if log_correlation:
@@ -103,7 +113,7 @@ async def io_task():
 @app.get("/cpu_task")
 async def cpu_task():
     for i in range(1000):
-        n = i*i*i
+        _ = i * i * i
     logging.error("cpu task")
     return "CPU bound task finish!"
 
@@ -136,16 +146,28 @@ async def chain(response: Response):
     logging.critical(headers)
 
     async with httpx.AsyncClient() as client:
-        await client.get(f"http://localhost:8000/", headers=headers,)
+        await client.get(
+            f"http://localhost:8000/",
+            headers=headers,
+        )
     async with httpx.AsyncClient() as client:
-        await client.get(f"http://{TARGET_ONE_HOST}:8000/io_task", headers=headers,)
+        await client.get(
+            f"http://{TARGET_ONE_HOST}:8000/io_task",
+            headers=headers,
+        )
     async with httpx.AsyncClient() as client:
-        await client.get(f"http://{TARGET_TWO_HOST}:8000/cpu_task", headers=headers,)
+        await client.get(
+            f"http://{TARGET_TWO_HOST}:8000/cpu_task",
+            headers=headers,
+        )
     logging.info("Chain Finished")
     return {"path": "/chain"}
+
 
 if __name__ == "__main__":
     # update uvicorn access logger format
     log_config = uvicorn.config.LOGGING_CONFIG
-    log_config["formatters"]["access"]["fmt"] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
+    log_config["formatters"]["access"][
+        "fmt"
+    ] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
     uvicorn.run(app, host="0.0.0.0", port=EXPOSE_PORT, log_config=log_config)
